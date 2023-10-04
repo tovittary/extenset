6fb3f162646cd98acce27f5fb9867ac9798d10f6
tovittary <tovittary@gmail.com>
2023-10-05 01:56:31 +0300

    Extension method for converting strings to enumeration values

---
+++ src/Extenset/String/Conversion/ToEnum.cs
@@ -0,0 +1,23 @@
+﻿namespace Extenset;
+
+/// <summary>
+/// Extension methods for <see cref="string" />.
+/// </summary>
+public partial class StringExtensions
+{
+    /// <summary>
+    /// Converts the <see cref="string" /> to a enum value.
+    /// </summary>
+    /// <param name="enumString">The <see cref="string" /> to convert.</param>
+    /// <param name="ignoreCase">Indicates whether to ignore case.</param>
+    /// <typeparam name="T">A type of enum.</typeparam>
+    /// <returns>The enum value generated from <see cref="string" />.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="enumString" /> is null.</exception>
+    /// <seealso cref="Enum.Parse(System.Type,string,bool)" />
+    public static T ToEnum<T>(this string enumString, bool ignoreCase = false)
+        where T : struct
+    {
+        if (enumString is null) throw new ArgumentNullException(nameof(enumString));
+        return (T)Enum.Parse(typeof(T), enumString, ignoreCase);
+    }
+}
---
+++ src/tests/Extenset.Tests/String/Conversion/ToEnumTests.cs
@@ -0,0 +1,57 @@
+﻿namespace Extenset.Tests;
+
+[TestFixture]
+internal sealed partial class StringExtensionsTests
+{
+    internal enum TestEnum
+    {
+        First,
+        Second,
+        Third
+    }
+
+    [Test]
+    public void ToEnum_EnumStringNull_ShouldThrow()
+    {
+        // Arrange
+        string? enumString = null;
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(() => enumString!.ToEnum<TestEnum>());
+    }
+
+    [TestCase("first", TestEnum.First)]
+    [TestCase("second", TestEnum.Second)]
+    [TestCase("third", TestEnum.Third)]
+    public void ToEnum_IgnoreCase_ValidEnumString_ShouldReturnValue(string enumString, TestEnum expected)
+    {
+        // Act
+        var actual = enumString.ToEnum<TestEnum>(true);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestCase("")]
+    [TestCase("fourth")]
+    public void ToEnum_IgnoreCase_InvalidEnumString_ShouldThrow(string enumString) =>
+        Assert.Throws<ArgumentException>(() => enumString.ToEnum<TestEnum>(true));
+
+    [TestCase("First", TestEnum.First)]
+    [TestCase("Second", TestEnum.Second)]
+    [TestCase("Third", TestEnum.Third)]
+    public void ToEnum_RegardCase_ValidEnumString_ShouldReturnValue(string enumString, TestEnum expected)
+    {
+        // Act
+        var actual = enumString.ToEnum<TestEnum>();
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestCase("")]
+    [TestCase("first")]
+    [TestCase("fourth")]
+    public void ToEnum_RegardCase_InvalidEnumString_ShouldThrow(string enumString) =>
+        Assert.Throws<ArgumentException>(() => enumString.ToEnum<TestEnum>());
+}
