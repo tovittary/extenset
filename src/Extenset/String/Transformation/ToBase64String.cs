d252883343d42df91d8e6335147b8b46b7300dfa
tovittary <tovittary@gmail.com>
2023-10-11 01:31:32 +0300

    Extension method for generating Base64 from strings

---
+++ src/Extenset/String/Transformation/ToBase64String.cs
@@ -0,0 +1,29 @@
+﻿namespace Extenset;
+
+using System.Text;
+
+/// <summary>
+/// Extension methods for <see cref="string" />.
+/// </summary>
+public static partial class StringExtensions
+{
+    /// <summary>
+    /// Generates a Base64 string from the <see cref="string" />.
+    /// </summary>
+    /// <param name="string">The <see cref="string" /> to generate a Base64 from.</param>
+    /// <param name="encoding">
+    /// The encoding used to generate a byte array from the <see cref="string" />.
+    /// If the parameter value is <c>null</c>, then <see cref="Encoding.UTF8" /> encoding is used.
+    /// </param>
+    /// <returns>The Base64 string generated from the <see cref="string" />.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="string" /> is <c>null</c>.</exception>
+    public static string ToBase64String(this string @string, Encoding? encoding = null)
+    {
+        if (@string is null) throw new ArgumentNullException(nameof(@string));
+
+        var encodingToUse = encoding ?? Encoding.UTF8;
+        var bytes = encodingToUse.GetBytes(@string);
+
+        return Convert.ToBase64String(bytes);
+    }
+}
---
+++ src/tests/Extenset.Tests/String/Transformation/ToBase64StringTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Extenset.Tests;
+
+using System.Text;
+
+internal sealed partial class StringExtensionsTests
+{
+    [Test]
+    public void ToBase64String_StringEmpty_ShouldReturnEmptyString()
+    {
+        // Arrange
+        var expected = string.Empty;
+        var emptyString = string.Empty;
+
+        // Act
+        var actual = emptyString.ToBase64String();
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void ToBase64String_StringNull_ShouldThrow()
+    {
+        // Arrange
+        string? nullString = null;
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(() => nullString!.ToBase64String());
+    }
+
+    [Test]
+    public void ToBase64String_Utf16Encoding_ShouldReturnUtf16Base64String()
+    {
+        // Arrange
+        const string expected = "dABlAHMAdAAgAHMAdAByAGkAbgBnAA==";
+        const string @string = "test string";
+
+        // Act
+        var actual = @string.ToBase64String(Encoding.Unicode);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void ToBase64String_ValidString_ShouldReturnUtf8Base64String()
+    {
+        // Arrange
+        const string expected = "dGVzdCBzdHJpbmc=";
+        const string @string = "test string";
+
+        // Act
+        var actual = @string.ToBase64String();
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+}
